9f6662db13f60d35f527ca93c7e8f27c128e0a2d
alangon03 <alangon03@gmail.com>
2023-06-18 09:45:52 -0700

    Make 1st test pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
 const repeatString = function() {
 
+    const stringChoice = "hey";
+    let n = 10;
+    let outputString = '';
+
+    while (n) {
+        outputString += stringChoice;
+        n--;
+    };
+    console.log(outputString);
+    return outputString;
 };
 
 // Do not edit below this line
