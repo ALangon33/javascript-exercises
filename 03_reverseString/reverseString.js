c7acf50fc5666291350c8e78f3c05564e384bd46
alangon03 <alangon03@gmail.com>
2023-06-18 11:56:52 -0700

    Pseudocode solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Check test and use input ^^^ here
+    // Set string length to variable
+    // Convert string to array
+    // Inverse array
+    // Reconstruct string
 };
 
 // Do not edit below this line
