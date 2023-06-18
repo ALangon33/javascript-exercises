5cdcb0513fa24d0d513ba28c9b33862ee23e9dec
alangon03 <alangon03@gmail.com>
2023-06-18 13:39:43 -0700

    Pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, valuesToRemove) {
+
+    // Assign arguments from input to variables
+    const args = Array.from(arguments);
+    const subjectArray = args[0];
+    const removalArray = args[1];
+    let removalLen;
+    // Get length for loop
+    let subjectLen = subjectArray.length;
+    if (typeof (removalArray) === 'number') {
+        removalLen = 1;
+    } else {
+        removalLen = removalArray.length;
+    }
+    const output = [];
+    let o = 0;
+
+    for (i=0; i < subjectLen;) {
+        for (n=0; n < removalLen; n++) {
+            if (subjectArray[i] !== removalArray && typeof (removalArray) === 'number') {
+                output[o] = subjectArray[i];
+                o++;
+                i++
+            } else if (subjectArray[i] === removalArray && typeof (removalArray) === 'number') {
+                i++;
+            } else {
+                i++;
+            };
+        };
+    };
+    return output;
 
 };
 
