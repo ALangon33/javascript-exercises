5c090aa7756c73fccb11fffb088e1a7f52efe206
alangon03 <alangon03@gmail.com>
2023-06-18 11:51:26 -0700

    Get confused, check solution, realize what's wrong, do it again

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,16 +1,16 @@
-const repeatString = function() {
-
-    const stringChoice = "hey";
-    let n = 10;
-    let outputString = '';
-
-    while (n) {
-        outputString += stringChoice;
-        n--;
+const repeatString = function (string, number) {
+    outputString = '';
+    // Creates output Array elements from each numArray element
+    if (number < 0) {
+        return "ERROR"
+    } else {
+        for (number; number > 0; --number) {
+            outputString += string;
+        };
+        return outputString;
     };
-    console.log(outputString);
-    return outputString;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
