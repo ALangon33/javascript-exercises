5e0e39f12f440ba3356e9aa81f1a2a82ab103883
alangon03 <alangon03@gmail.com>
2023-06-18 12:13:08 -0700

    Pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,19 @@
 const reverseString = function(string) {
     // Check test and use input ^^^ here
+
     // Set string length to variable
+    let strLen = string.length;
+    // Create output string
+    let output = '';
     // Convert string to array
+    const strArray = string.split("");
     // Inverse array
+    const reverseArray = strArray.reverse();
     // Reconstruct string
+    for (letters of reverseArray) {
+        output += letters;
+    }
+    return output;
 };
 
 // Do not edit below this line
