0cc85485fe3b095f8f0c093c76b1ac6dbe71384e
alangon03 <alangon03@gmail.com>
2023-06-18 20:15:30 -0700

    Pass all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,18 +1,13 @@
 const leapYears = function(x) {
-
-    // if modulous 400 return true
     if (x % 400 === 0 ) {
         return true;
     } else if (x % 100 === 0) {
-        return false;
+    return false;
     } else if (x % 4 === 0) {
-        return true;
+    return true;
     } else {
         return false;
     };
-    // else if Modulous 4 && !100 true
-    // else false 
-
 };
 
 // Do not edit below this line
