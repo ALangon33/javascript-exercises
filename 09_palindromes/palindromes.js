3458a9709e38f8aaa3a4323fd25d77ffb93cb8bd
alangon03 <alangon03@gmail.com>
2023-06-18 21:29:38 -0700

    Pass half of tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const lowerCaseString = string.toLowerCase();
+    const illegalChars = ['!', ',', '.', ' '];
+    // Break string into array
+    const array = lowerCaseString.split("");
+    // remove non-letter characters (! , . etc) 
+    const filteredArray = array.filter(x => !illegalChars.includes(x));
+    // combine and assign to var
+    const originalArray = filteredArray.join('');
+    // then invert and combine to compare
+    const inverseArray = filteredArray.reverse();
+    const inverseCombined = inverseArray.join('');
+    // if equal true, else false
+    if (originalArray === inverseCombined) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
