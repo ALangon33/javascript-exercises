0321aa1c2652770354d97e13084597eac8db2aec
alangon03 <alangon03@gmail.com>
2023-06-18 22:04:42 -0700

    Pass first test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    // Take input value and increment up the
+    // Fibonacci sequence until input value is reached
+    let oldestValue = 1;
+    let currentValue = 1;
+    let nextValue = 0;
 
+    if (typeof (n) === 'string') {
+        Number(n);
+    };
+
+    if (n === 1 || n === 2) {
+        return 1;
+    } else {
+        for (i=2; i<n; i++) {
+            nextValue = currentValue + oldestValue;
+            currentValue = nextValue;
+        };
+    };
+
+    return nextValue;
 };
 
 // Do not edit below this line
